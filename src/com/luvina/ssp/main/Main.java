2090046c0c7e5905d637718a38a83a2ca923e378
Stuart <duststuart@gmail.com>
2018-04-28 03:36:59 +0700

    Main method
    - try BufferedReader obj with Keyboard

--- src/com/luvina/ssp/main/Main.java
+++ src/com/luvina/ssp/main/Main.java
@@ -1,9 +1,8 @@
 package com.luvina.ssp.main;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
@@ -13,22 +12,48 @@ public class Main {
 		// Client client = new Client();
 		// client.initClient("LocalHost", 4499);
 
-		BufferedReader reader = null;
+		// BufferedReader reader = null;
+		//
+		// try {
+		// File file = new File("D:/Ann/SLA.sign");
+		// reader = new BufferedReader(new FileReader(file));
+		//
+		// String line;
+		// while ((line = reader.readLine()) != null) {
+		// System.out.println(line.length());
+		// }
+		//
+		// reader.close();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
 
+		System.out.print("Enter your name: ");
+		InputStreamReader reader = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(reader);
+		String name = "";
 		try {
-			File file = new File("D:/Ann/SLA.sign");
-			reader = new BufferedReader(new FileReader(file));
-
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line.length());
-			}
-
-			reader.close();
+			name = in.readLine();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		System.out.println("Hello, " + name + ". Enter three ints...");
+		int[] values = new int[3];
+		double sum = 0.0;
+		for (int i = 0; i < values.length; i++) {
+			System.out.print("Number " + (i + 1) + ": ");
+			String temp = "";
+			try {
+				temp = in.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			values[i] = Integer.parseInt(temp);
+			sum += values[i];
+		}
+		System.out.println("The average equals " + sum / values.length);
 	}
 
 }
