12cceb3555a2e8825f78076471648ed07576f459
Stuart <duststuart@gmail.com>
2018-04-28 03:35:14 +0700

    Main method
    - try BufferedReader program

--- src/com/luvina/ssp/main/Main.java
+++ src/com/luvina/ssp/main/Main.java
@@ -1,15 +1,34 @@
 package com.luvina.ssp.main;
 
-import com.luvina.ssp.serverclient.Client;
-import com.luvina.ssp.serverclient.Server;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Server server = new Server();
-		server.initServer(4499);
-		Client client = new Client();
-		client.initClient("LocalHost", 4499);
+		// Server server = new Server();
+		// server.initServer(4499);
+		// Client client = new Client();
+		// client.initClient("LocalHost", 4499);
+
+		BufferedReader reader = null;
+
+		try {
+			File file = new File("D:/Ann/SLA.sign");
+			reader = new BufferedReader(new FileReader(file));
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line.length());
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
